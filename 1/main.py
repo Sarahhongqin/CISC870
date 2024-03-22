23fd86167fa73aa2f9596ed55780a9eeb4b48d4c
hongqin <qinhong630@gmail.com>
2024-03-21 21:41:24 -0400

    Updated main.py for time counting

--- 1/main.py
+++ 1/main.py
@@ -2,41 +2,11 @@
 from target_combination import generate_target_combinations
 from test_generator import TestGenerator
 from parser import parse_parameters, parse_constraints
-
-# def is_combination_valid(combination, constraints):
-#     # Check if a given combination violates any constraint
-#     # print('combination:', combination)
-#     for constraint in constraints:
-#         # print('constraint:', constraint)
-#         if all(combination.get(param) == value for param, value in constraint):
-#             return False
-#     return True
-
-# def generate_target_combinations(parameters, constraints):
-#     from itertools import combinations
-
-#     # Generate all pairs of parameters for coverage strength t = 2
-#     parameter_pairs = list(combinations(sorted(parameters), 2))
-#     # print(parameter_pairs)
-
-#     # Generate all valid combinations for each pair
-#     target_combinations = []
-#     for param1, param2 in parameter_pairs:
-#         values1 = parameters[param1]
-#         values2 = parameters[param2]
-
-#         # Generate all possible combinations for this pair
-#         for value1 in values1:
-#             for value2 in values2:
-#                 combination = {param1: value1, param2: value2}
-
-#                 # Check if the combination is valid against the constraints
-#                 if is_combination_valid(combination, constraints):
-#                     target_combinations.append(combination)
-
-#     return target_combinations
+import time
 
 def main(input_text):
+
+    start_time = time.time()
 
     parameters = parse_parameters(input_text)
     constraints = parse_constraints(input_text)
@@ -49,9 +19,14 @@ def main(input_text):
     
     # Generate the test cases
     test_suite = test_generator.generate_test_cases()
+
+    end_time = time.time()
+    time_taken = end_time - start_time
     print("The test suite:\n")
     for i, test_case in enumerate(test_suite.test_cases, 1):
         print(f"Test Case {i}: {test_case}")
+
+    print("\nTime taken to generate the test suite: {:.2f} seconds".format(time_taken))
 
 
 # Run the main function if this script is executed
